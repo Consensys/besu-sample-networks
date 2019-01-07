fc43512f05d46741c6c27486a3aed3d955b6b7ea
Nicolas MASSART <nicolas.massart@consensys.net>
2019-01-07 16:59:41 +0100

    Use a fixed version and enable any host
    - add fixed version of Pantheon and use it for images and centralise it in an
    .env file for docker-compose and also source it in sh scripts
    - add --host-whitelist=*
    - add commands to remove images in the remove script

--- run.sh
+++ run.sh
@@ -11,6 +11,8 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+. .env
+
 me=`basename "$0"`
 DEFAULT_SCALING=4
 scaleNode=$DEFAULT_SCALING
