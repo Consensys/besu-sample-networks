dd19a9b8941fbfea9817c5c8ed3e5c012ee5c352
Nicolas MASSART <NicolasMassart@users.noreply.github.com>
2019-01-08 14:49:39 +0100

    add script to install docker, docker compose and run quickstart on azure linux vm (#7)
    have to add this script to test Azure deployment

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+USER=$1
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+cd /home/$USER
+git clone https://github.com/PegaSysEng/pantheon-quickstart.git
+cd pantheon-quickstart
+sudo ./run.sh -p 80
