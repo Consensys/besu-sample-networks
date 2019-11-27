20e9991b54dbb707c1c3c713444b0dbf31ef4e9b
Joshua Fernandes <joshua.fernandes@consensys.net>
2019-11-27 11:23:52 +1000

    privacy and support for native ports
    Signed-off-by: Joshua Fernandes <joshua.fernandes@consensys.net>

--- README.md
+++ README.md
@@ -1,11 +1,66 @@
+# Quickstart
+
+## Prerequisites
+
+To run this tutorial, you must have the following installed:
+
+- MacOS or Linux 
+   
+    !!! important 
+        The Private Network Quickstart is not supported on Windows. If using Windows, run the quickstart
+        inside a Linux VM such as Ubuntu. 
+
+- [Docker and Docker-compose](https://docs.docker.com/compose/install/) 
+
+| ⚠️ **Note**: If on MacOS, please ensure that you allow docker to use upto 4G of memory under the _Resources_ section. The docker [site](https://docs.docker.com/docker-for-mac/) has details on how to do this |
+| --- |
+
+
+## For Blockchain quickstarts without privacy (orion):
 For complete and up-to-date documentation, please see [Docker quick-start, on the Besu documentation site](https://besu.hyperledger.org/Tutorials/Quickstarts/Private-Network-Quickstart/).
 
+## For Blockchain quickstarts without privacy (orion) enables:
 
-curl -X POST "http://10.57.5.245:32773/kibana/api/saved_objects/index-pattern/besu" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'
-{
-  "attributes": {
-    "title": "besu-*",
-    "timeFieldName": "@timestamp"
-  }
-}
-'
+`./run-privacy.sh` creates docker images for configuring a network of
+Besu nodes as well as Orion nodes which include 3 nodes with privacy
+enabled.
+Where the node details are as follows:
+
+Name  | Besu Node address                      | Orion node key | Node URL
+----- | ---- | ---- | ---- |
+node1 | 0x866b0df7138daf807300ed9204de733c1eb6d600 | 9QHwUJ6uK+FuQMzFSXIo7wOLCGFZa0PiF771OLX5c1o= | http://localhost:20000
+node2 | 0xa46f0935de4176ffeccdeecaf3c6e3ca03e31b22 | qVDsbJh2UluZOePxbXAL49g0S0s2gGlJ3ftQceMlchU= | http://localhost:20002
+node3 | 0x998c8bc11c28b667e4b1930c3fe3c9ab1cde3c52 | T1ItOQxwgY1pTW6YXb2EbKXYkK4saBEys3CfJ2OIKHs= | http://localhost:20004
+
+
+### Use `eeajs` to deploy contracts
+#### Prerequisites
+ - [Nodejs](https://nodejs.org/en/download/)
+ 
+ Install the following after downloading `Nodejs` - 
+ - [web3](https://www.npmjs.com/package/web3)
+ - [axios](https://www.npmjs.com/package/axios)
+  
+ Clone [eeajs](https://github.com/PegaSysEng/web3js-eea) github repo. 
+ 
+#### EventEmitter contract
+
+After the containers from above have started, execute `node example/eventEmitter.js` in the `web3js-eea` project that you have just cloned
+This deploys the `EventEmitter` contract, sets a value of `1000` and gets the value.
+
+It can be verified from the output of the last transaction - `0x00000000000000000000000000000000000000000000000000000000000003e8`
+which is the hex value of `1000`.
+
+#### ERC20 token
+
+Executing `node example/erc20.js` deploys a `HumanStandardToken` contract and transfers 1 token to node2.
+
+This can be verified from the `data` field of the `logs` which is `1`.
+
+### Stop Services and Network
+`./stop.sh` stops all the docker containers created.
+
+### Remove stopped containers and volumes 
+`./remove.sh` stops and removes all the containers and volumes. 
+
+
--- list.sh
+++ list.sh
@@ -28,29 +28,47 @@ echo "----------------------------------"
 
 # Displays services list with port mapping
 docker-compose ps
+dots=""
+maxRetryCount=50
+while [ "$(curl -m 10 -s -o /dev/null -w ''%{http_code}'' http://${HOST}:5601/api/status)" != "200" ] && [ ${#dots} -le ${maxRetryCount} ]
+do
+  dots=$dots"."
+  printf "Kibana is starting, please wait $dots\\r"
+  sleep 10
+done
+echo "Setting up the besu index pattern in kibana"
+curl -X POST "http://${HOST}:5601/api/saved_objects/index-pattern/besu" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '{"attributes": {"title": "besu-*","timeFieldName": "@timestamp"}}'
+
+if [ -z `docker-compose -f docker-compose_privacy.yml ps -q orion3` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose -f docker-compose_privacy.yml ps -q orion3)` ]; then
+  echo "Orion not running, skipping the orion index pattern in kibana."
+else
+  echo "\nSetting up the orion index pattern in kibana"
+  curl -X POST "http://${HOST}:5601/api/saved_objects/index-pattern/orion" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '{"attributes": {"title": "orion-*","timeFieldName": "@timestamp"}}'
+fi
 
 # Get individual port mapping for exposed services
 explorerMapping=`docker-compose port explorer 80`
-
+explorerPort="${explorerMapping##*:}"
 dots=""
 maxRetryCount=50
-while [ "$(curl -m 1 -s -o /dev/null -w ''%{http_code}'' http://${HOST}:${explorerMapping##*:})" != "200" ] && [ ${#dots} -le ${maxRetryCount} ]
+while [ "$(curl -m 10 -s -o /dev/null -w ''%{http_code}'' http://${HOST}:${explorerPort})" != "200" ] && [ ${#dots} -le ${maxRetryCount} ]
 do
   dots=$dots"."
   printf "Block explorer is starting, please wait $dots\\r"
-  sleep 1
+  sleep 10
 done
 
 echo "****************************************************************"
 if [ ${#dots} -gt ${maxRetryCount} ]; then
-  echo "ERROR: Web block explorer is not started at http://${HOST}:${explorerMapping##*:}$ !"
+  echo "ERROR: Web block explorer is not started at http://${HOST}:${explorerPort} !"
   echo "****************************************************************"
 else
-  echo "JSON-RPC HTTP service endpoint      : http://${HOST}:${explorerMapping##*:}/jsonrpc"
-  echo "JSON-RPC WebSocket service endpoint : ws://${HOST}:${explorerMapping##*:}/jsonws"
-  echo "GraphQL HTTP service endpoint       : http://${HOST}:${explorerMapping##*:}/graphql"
-  echo "Web block explorer address          : http://${HOST}:${explorerMapping##*:}"
-  echo "Prometheus address                  : http://${HOST}:${explorerMapping##*:}/prometheus/graph"
-  echo "Grafana address                     : http://${HOST}:${explorerMapping##*:}/grafana-dashboard"
+  echo "JSON-RPC HTTP service endpoint      : http://${HOST}:8545"
+  echo "JSON-RPC WebSocket service endpoint : ws://${HOST}:8546"
+  echo "GraphQL HTTP service endpoint       : http://${HOST}:8547"
+  echo "Web block explorer address          : http://${HOST}:${explorerPort}/"
+  echo "Prometheus address                  : http://${HOST}:9090/graph"
+  echo "Grafana address                     : http://${HOST}:3000/d/XE4V0WGZz/besu-overview?orgId=1&refresh=10s&from=now-30m&to=now&var-system=All"
+  echo "Kibana address                     : http://${HOST}:5601/app/kibana#/discover"
   echo "****************************************************************"
 fi
